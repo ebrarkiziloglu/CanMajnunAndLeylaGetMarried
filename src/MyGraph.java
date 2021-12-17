5e46ead814aa7eef9b9620fdc6aa977f2039db4f
Asude Ebrar Kızıloğlu <83813907+ebrarkiziloglu@users.noreply.github.com>
2021-12-17 13:34:26 +0300

    Update MyGraph.java

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -1,12 +1,10 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-
-import static java.lang.System.out;
-// This is the implementation for weighted and directed graph of the country of Leyla and Mecnun.
-// The data given in the input file will be stored in an adjacency list field.
-// In the main class, two different objects of this class will be created.
-// First object, graph1, is used for the one side of the country -for 'c' cities-.
-// Second object, graph2, is used for the other side of the country -for Leyla's home town and all 'd' cities- and it will be undirected.
+//This is the implementation for weighted and directed graph of the country of Leyla and Mecnun.
+//The data given in the input file will be stored in an adjacency list field.
+//In the main class, two different objects of this class will be created.
+//First object, graph1, is used for the one side of the country -for 'c' cities-.
+//Second object, graph2, is used for the other side of the country -for Leyla's home town and all 'd' cities- and it will be undirected.
 
 public class MyGraph {
 
@@ -19,35 +17,38 @@ public class MyGraph {
     private int[] shortDistance;
     int leylasHometown;
     int mecnunsHometown;
+    
     // Constructors:
     public MyGraph(int v, int mecnun, int leyla) {
         this.numOfVertices = v;
         adjList = new ArrayList<HashMap<Integer, Integer>>();
-        // now v+1 empty hashmap will be created in adjList.
+        // now (Leyla's home id + 1) empty hash map will be created in adjList.
         // Since our nodes' index starts with 1, 0th element of the adjList will not be used, yet created to protect the indexing.
-        for(int i = 0; i <=v; i++){
+        for(int i = 0; i <=leyla; i++){
             adjList.add(new HashMap<Integer, Integer>());
         }
         this.mecnunsHometown = mecnun;
         this.leylasHometown = leyla;
     }
-    // the Constructor below will be used for the second part of the country: 
-    		// there is no specific index for Mecnun's home town. 
-    		// And for Leyla's home town, index 0 will be used.
+    // the Constructor below will be used for the second part of the country:
+    // there is no index for Mecnun's home town, since it is irrelevant.
+    // And for Leyla's home town, index 0 will be used.
     public MyGraph(int v){
-        this.numOfVertices = v;
+        this.numOfVertices = v;		// v equals the number of d cities + 1 (Leyla's home town)
         adjList = new ArrayList<HashMap<Integer, Integer>>();
-        // Since 0th index of the adjList will be used by Leyla's home town city, so the indexing will be fixed.
-        for(int i = 0; i <=v; i++){
+        // Since 0th index of the adjList will be used by Leyla's home town city, the indexing will be fixed.
+        // (dI)th city will owns the Ith Hash map in the adjacency list.
+        for(int i = 0; i <v; i++){
             adjList.add(new HashMap<Integer, Integer>());
         }
     }
+    
     // Getters & Setters:
-    public int getNumOfVertices() {  return numOfVertices;  }
     public int[] getPreviousNodes() {   return this.prevNodes;  }
     public int[] getShortDistanceToMecnun() {    return this.shortDistance;  }
-    public int getLeylasHometown() {  return leylasHometown;    }
-    public int getMecnunsHometown(){  return mecnunsHometown;   }
+    public ArrayList<HashMap<Integer, Integer>> getAndjList()	{ return this.adjList;	  }
+    // get all neighbors and weights of the edges, given the specific index of a node
+    public HashMap<Integer, Integer> getAllNeighbors(int source){   return this.adjList.get(source); }
 
     // add new weighted edges:
     public void addEdge(int source, int sink, int weight){
@@ -57,16 +58,11 @@ public class MyGraph {
                 this.adjList.get(source).put(sink, weight);
         } else { // the edge between the nodes does not exist yet:
             this.adjList.get(source).put(sink, weight);
-        }
-    }
-    // get all neighbors and weights of the edges,given the specific index of a node
-    public HashMap<Integer, Integer> getAllNeighbors(int source){
-        return this.adjList.get(source);
-    }
+        }  }
 
-    // Dijkstra's Algorithm:
-    // Finding whether Mecnun will arrive in time:  	 //	takes the index of the starting city, i.e. Mecnun's hometown, as a parameter.
-    													 // returns the list of previous nodes of each node.
+    // PART 1: Dijkstra's Algorithm:
+    // Finding whether Mecnun will arrive in time:  	 //	takes the index of the starting city, i.e. Mecnun's home town, as a parameter.
+    // returns the list of previous nodes of each node.
     public void findShortestPaths(int source){
         int initial = Integer.MAX_VALUE;
         this.prevNodes = new int[this.numOfVertices + 1];
@@ -75,16 +71,15 @@ public class MyGraph {
         this.shortDistance[0] = -2;
         for(int i = 1; i <= this.numOfVertices; i++){
             this.prevNodes[i] = -1;
-            this.shortDistance[i] = initial;
-        }
+            this.shortDistance[i] = initial;       }
         prevNodes[source] = 0;
         shortDistance[source] = 0;
-
         boolean[] visited = new boolean[this.numOfVertices+1];
         for(int i = 0; i <= this.numOfVertices; i++) visited[i] = false;
         visited[0] = true;
-        for(int i = 1; i <= this.numOfVertices; i++){
-            int current = nextNode(visited);
+        // for(int i = 1; i <= this.numOfVertices; i++){
+        for(int i = 0; i <= this.numOfVertices; i++){
+            int current = nextNode(visited, shortDistance, this.numOfVertices);
             if(this.shortDistance[current] != initial){
                 HashMap<Integer, Integer> edges = this.getAllNeighbors(current);
                 // int numOfEdges = edges.size();
@@ -94,79 +89,104 @@ public class MyGraph {
                         if(shortDistance[current] + weight < shortDistance[neighbor]){
                             shortDistance[neighbor] = shortDistance[current] + weight;
                             this.prevNodes[neighbor] = current;
-                        }
-                    }
-                }
-            }
-            visited[current] = true;
-        }
-    }
+                        }   }   }
+                visited[current] = true;
+            }   }   }
 
-    // To find which node to visit next, we need to determine the unvisited node with the shortest distance to the source node (Mecnun's city).
-    public int nextNode(boolean[] visited){
+    // To find which node to visit next:
+    // In Part 1, we need to determine the unvisited node with the shortest distance to the source node (Mecnun's city).
+    // In Part 2, we need to determine the unvisited node with the least current cost.
+    public int nextNode(boolean[] visited, int[] distance, int count){
+    	// In order to use the same "find the Node with the min cost/distance that is not yet visited" function 
+    	// for both part 1 and 2 of the Project, 3 parameters are needed.
+    	// For Part 1, visited boolean list, distances to the source node list, and number of vertices are given as parameters.
+    	// For Part 2, visited boolean list, cost of each node list, and number of vertices are given as parameters.
         int minDistance = Integer.MAX_VALUE;
         int minIndex = -1;
         boolean notVisited = true;
-        for(int i = 1; i <= this.numOfVertices; i++){
+        for(int i = 0; i <= count; i++){
             if(!visited[i]){        // returns true if the node is not visited yet.
-                if(notVisited) 
-                    minIndex = i; 
-                notVisited = false;
-                if(this.shortDistance[i] < minDistance){
-                    minDistance = this.shortDistance[i];
+                if(notVisited)
                     minIndex = i;
-                }
-            }
-        }
+                notVisited = false;
+                if(distance[i] < minDistance){
+                    minDistance = distance[i];
+                    minIndex = i;
+                }   }   }
         return minIndex;
     }
 
-
+    // After the Dijkstra's Algorithm, this function returns the shortest path String in the right form, to be directly printed to the output file.
     public String mecnunsPathToLeyla(){
         String path = "";
         int parent = this.prevNodes[leylasHometown];
         if(parent == -1) return "-1";
-        
         ArrayList<Integer> pathIndex = new ArrayList<>();
         pathIndex.add(leylasHometown);
-        
+
         while(parent != this.mecnunsHometown){
-        	pathIndex.add(parent);
+            pathIndex.add(parent);
             parent = this.prevNodes[parent];
         }
         pathIndex.add(parent);
         int length = pathIndex.size();
-        for(int i = length-1; i > -1; i--){
+        for(int i = length-1; i > 0; i--){
             path += "c" + pathIndex.get(i) + " ";
         }
+        path += "c" + pathIndex.get(0);
         return path;
     }
 
-    // Prim's algorithm:
+    // PART 2: Prim's algorithm:
     // Determining a honeymoon route, if possible, for Leyla and Mecnun:
     // returns the cost of the honeymoon
     public int findHoneymoonRoute(){
-    	int source = 0;		// Leyla's home town index in graph2.
-        int cost = 0;
+        // Leyla's home town index, which is the source index, is 0 in graph2.
+        int totalCost = 0;
+        int initial = Integer.MAX_VALUE;
+        boolean[] visited = new boolean[this.numOfVertices];
+        int[] parent = new int[this.numOfVertices];
+        int[] cost = new int[this.numOfVertices];
+        for(int i = 0; i <this.numOfVertices; i++) {
+            visited[i] = false;
+            parent[i] = -1;
+            cost[i] = initial;
+        }
+        cost[0] = 0;
+        for(int i = 0; i < this.numOfVertices; i++){
+            int current = nextNode(visited, cost, this.numOfVertices-1);
+            if(cost[current] != initial){
+                HashMap<Integer, Integer> edges = this.getAllNeighbors(current);
+                for(int neighbor : edges.keySet()){
+                    if(!visited[neighbor]){         // means this neighbor node is not visited yet
+                        int weight = edges.get(neighbor);
+                        if(weight < cost[neighbor]){
+                            cost[neighbor] = weight;
+                            parent[neighbor] = current;
+                        }   }   }
+                visited[current] = true;
+            }   }
+        // check whether all the edges are visited:
+        boolean allNodesVisited = true;
+        for(boolean b : visited) if(!b) allNodesVisited = false;
 
+        // Calculate the cost:
+        if(allNodesVisited) {
+            for (int edgeCost : cost) totalCost += edgeCost;
+            totalCost *= 2;
+        } else{
+            totalCost = -2;
+        }
 
-
-
-        // main implementation
-
-
-        // check edge cases
-
-        // if Mecnun cannot make it in time, return -1  ??
-        // if no route is possible, return -2   ??
-
-        return cost;
+        return totalCost;
     }
 
+    // Following method is put for design and debugging purposes. 
     @Override
     public String toString(){
         String text = "";
-        for(int i = 0; i <= numOfVertices; i++){
+        int length = this.adjList.size();
+        for(int i = 0; i < length; i++){
             text += "source: " + i + ":  ";
             HashMap<Integer,Integer> edges = this.adjList.get(i);
             for(int neighbor : edges.keySet())
